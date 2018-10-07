462162cd0eef8f2c7fae8693a8f240883fbe5b5c
Adrian Corcoran <adrian.corcoran@shopify.com>
2018-10-07 23:40:45 +0100

    Add first component

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-// let's go!
-console.log("hi there");
+import React from "react";
+import { render } from "react-dom";
+import StorePicker from "./components/StorePicker";
+
+render(<StorePicker />, document.querySelector("#main"));
