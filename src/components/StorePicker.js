964cd3551f7b0ca8126f6fb5ddca996919c3f194
Adrian Corcoran <adrian.corcoran@shopify.com>
2018-10-09 23:29:58 +0100

    Create React Fragment

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,16 @@ import React from "react";
 
 class StorePicker extends React.Component {
   render() {
-    return <p>I am the Store Picker!</p>;
+    return (
+      <React.Fragment>
+        {/* my first component */}
+        <form className="store-selector">
+          <h2>Please Enter A Store</h2>
+          <input type="text" required placeholder="Store Name" />
+          <button type="submit">Visit Store -></button>
+        </form>
+      </React.Fragment>
+    );
   }
 }
 
