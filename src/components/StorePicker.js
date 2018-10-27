a55e03c54fd99d4452944543e8261dec039e2950
Adrian Corcoran <adrian.corcoran@shopify.com>
2018-10-28 00:39:20 +0100

    Video 10 Complete: Include helper functions

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
   render() {
@@ -7,7 +8,12 @@ class StorePicker extends React.Component {
         {/* my first component */}
         <form className="store-selector">
           <h2>Please Enter A Store</h2>
-          <input type="text" required placeholder="Store Name" />
+          <input
+            type="text"
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
           <button type="submit">Visit Store -></button>
         </form>
       </React.Fragment>
